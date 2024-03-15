2b37ccdf26600c3a8247f98b3e06362d1fe480f4
F2258487 Daniel Thomas Arcanjo Dourado <daniel.dourado@bb.com.br>
2024-03-15 15:15:41 -0300

    feat: add not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app.routing';
     AppRoutingModule,
     BrowserModule,
     PhotosModule,
+    ErrorsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
