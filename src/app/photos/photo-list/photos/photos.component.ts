ee34c35e4f6a002fc197263e41b656bb8d993a14
F2258487 Daniel Thomas Arcanjo Dourado <daniel.dourado@bb.com.br>
2024-03-18 11:26:07 -0300

    fix: correct gridview and use OnChanges

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { PhotoData } from '../../model/photo';
 
 @Component({
@@ -6,12 +6,23 @@ import { PhotoData } from '../../model/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
   @Input() photos: PhotoData[] = [];
+  rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos)
+      this.rows = this.groupCols(this.photos);
+  }
+
+  groupCols(photos: PhotoData[]): any[] {
+    const newRows = [];
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+    return newRows;
   }
 
 }
