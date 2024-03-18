05fb4507f3baf15681762fc9c550b91763e2924b
F2258487 Daniel Thomas Arcanjo Dourado <daniel.dourado@bb.com.br>
2024-03-18 12:39:28 -0300

    feat: use resolver to prevent leaking error message

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoData } from '../model/photo';
+import { PhotoData, PhotoProps } from '../model/photo';
 import { PhotoService } from '../service/photo.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -11,46 +11,39 @@ import { ActivatedRoute } from '@angular/router';
 export class PhotoListComponent implements OnInit {
 
   photos: PhotoData[] = [
-    new PhotoData({
-      authorId: 'id',
-      allowComments: true,
-      comments: 0,
-      likes: 0,
-      createdAt: new Date(),
-      description: 'Leao',
-      id: 'photoId',
-      url: 'https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?rs=1&pid=ImgDetMain'
+    // new PhotoData({
+    //   authorId: 'id',
+    //   allowComments: true,
+    //   comments: 0,
+    //   likes: 0,
+    //   createdAt: new Date(),
+    //   description: 'Leao',
+    //   id: 'photoId',
+    //   url: 'https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?rs=1&pid=ImgDetMain'
 
-    }),
-    new PhotoData({
-      authorId: 'id',
-      allowComments: true,
-      comments: 0,
-      likes: 0,
-      createdAt: new Date(),
-      description: 'Tigrinho',
-      id: 'photoId',
-      url: 'https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?rs=1&pid=ImgDetMain'
-    })
+    // }),
+    // new PhotoData({
+    //   authorId: 'id',
+    //   allowComments: true,
+    //   comments: 0,
+    //   likes: 0,
+    //   createdAt: new Date(),
+    //   description: 'Tigrinho',
+    //   id: 'photoId',
+    //   url: 'https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?rs=1&pid=ImgDetMain'
+    // })
   ];
 
   searchFilter: string = '';
 
   constructor(
-    private readonly photoService: PhotoService,
     private activatedRoute: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.photoService.getUserPhotos(this.activatedRoute.snapshot.params.username)
-      .subscribe(res => {
-        this.photos = [];
-        res.forEach(props => {
-          this.photos.push(PhotoData.fromApi(props));
-        });
-      },
-        err => console.error(err.message)
-      );
+    this.activatedRoute.snapshot.data.photos.forEach(
+      photoProps => this.photos.push(PhotoData.fromApi(photoProps as PhotoProps))
+    );
   }
 
 }
---
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PhotoData, PhotoProps } from '../model/photo';
+import { PhotoService } from '../service/photo.service';
+
+@Injectable({ providedIn: 'root' })
+export class PhotoListResolver implements Resolve<Observable<PhotoProps[]>> {
+  constructor(private service: PhotoService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PhotoProps[]> | Observable<Observable<PhotoProps[]>> | Promise<Observable<PhotoProps[]>> {
+    const username = route.params.username;
+    return this.service.getUserPhotos(username);
+  }
+
+}
