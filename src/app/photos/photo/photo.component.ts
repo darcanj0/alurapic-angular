431396381851bc67b133276b4d821be400c66667
F2258487 Daniel Thomas Arcanjo Dourado <daniel.dourado@bb.com.br>
2024-03-21 10:27:25 -0300

    fix: correct photos urls

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import EnvVariables from 'src/app/config/http';
 
 @Component({
   selector: 'ap-photo',
@@ -8,6 +9,20 @@ export class PhotoComponent {
   @Input()
   description: String = 'Lion';
 
+  private _url = 'https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?rs=1&pid=ImgDetMain';
+
+  public get url() {
+    return this._url;
+  }
+
   @Input()
-  url = 'https://th.bing.com/th/id/OIP.Xj_XB9KGp8Oon6kxGo69OgHaE-?rs=1&pid=ImgDetMain';
+  public set url(value) {
+    const notDataUri = !value.startsWith('data');
+    console.log(notDataUri);
+    if (notDataUri) {
+      this._url = EnvVariables.API_BASE_URL + 'imgs/' + value;
+    } else {
+      this._url = value;
+    }
+  }
 }
